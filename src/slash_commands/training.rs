a80f5d87c0445cb3708d66421ee6c26825e0deb2
Pascal H <p.harprecht@gmail.com>
2022-01-07 17:10:47 +0100

    training download

--- src/slash_commands/training.rs
+++ src/slash_commands/training.rs
@@ -6,14 +6,14 @@ use crate::{
     embeds::CrossroadsEmbeds,
     log::*,
     signup_board, status,
-    utils::DEFAULT_TIMEOUT,
+    utils::DEFAULT_TIMEOUT, data,
 };
 use chrono::{NaiveDate, NaiveDateTime};
 use serde::{ser::{SerializeSeq, SerializeStruct}, Serialize};
 use serenity::{
     builder::{CreateApplicationCommand, CreateEmbed},
     client::Context,
-    futures::future, model::{guild::Member, misc::Mention}, http::AttachmentType,
+    futures::future, model::{guild::{Member, PartialGuild, Role}, misc::Mention, id::RoleId}, http::AttachmentType,
 };
 use serenity::model::interactions::{
         application_command::{
@@ -336,11 +336,20 @@ enum DonwloadFormat {
     Csv,
 }
 
+impl std::fmt::Display for DonwloadFormat {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Csv => write!(f, "csv"),
+            Self::Json => write!(f, "json"),
+        }
+    }
+}
+
 #[derive(Serialize)]
-struct SignupData<'a> {
+struct SignupData {
     user: db::User,
     member: Member,
-    roles: Vec<&'a db::Role>,
+    roles: Vec<String>, // we only need the repr here
     comment: Option<String>
 }
 
@@ -362,19 +371,27 @@ struct SignupDataCsv<'a> {
 }
 
 #[derive(Serialize)]
-struct TrainingData<'a> {
+struct TrainingData {
     training: db::Training,
     available_roles: Vec<db::Role>,
-    signups: Vec<SignupData<'a>>,
+    signups: Vec<SignupData>,
 }
 
-struct DownloadData<'a> {
+#[derive(Serialize)]
+struct TierData {
+    id: i32,
+    name: String,
+    includes: Vec<Role>
+}
+
+struct DownloadData {
     output: DonwloadFormat,
     created: NaiveDateTime,
-    trainings: Vec<TrainingData<'a>>,
+    trainings: Vec<TrainingData>,
+    tiers: Vec<TierData>,
 }
 
-impl DownloadData<'_> {
+impl DownloadData {
 
     fn serialize_csv<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
@@ -392,7 +409,7 @@ impl DownloadData<'_> {
                     training_name: &t.training.title,
                     roles: &s.roles
                         .iter()
-                        .map(|r| r.repr.clone())
+                        .map(|r| r.as_str())
                         .collect::<Vec<_>>()
                         .join(", "),
                     comment: s.comment.as_deref(),
@@ -411,11 +428,12 @@ impl DownloadData<'_> {
             let mut elem = serializer.serialize_struct("data", 2)?;
             elem.serialize_field("created", &self.created)?;
             elem.serialize_field("trainings", &self.trainings)?;
+            elem.serialize_field("tiers", &self.tiers)?;
             elem.end()
     }
 }
 
-impl Serialize for DownloadData<'_> {
+impl Serialize for DownloadData {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: serde::Serializer {
@@ -431,6 +449,16 @@ async fn download(
     aci: &ApplicationCommandInteraction,
     option: &ApplicationCommandInteractionDataOption,
 ) -> LogResult<()> {
+
+    let guild_id = match ctx.data.read().await.get::<data::ConfigValuesData>() {
+        Some(conf) => conf.main_guild_id,
+        None => return LogError::new_slash("Guild configuration could not be loaded", aci.clone()).into(),
+    };
+
+    let guild = PartialGuild::get(ctx, guild_id)
+        .await
+        .log_slash_reply(aci)?;
+
     // Get subcommands
     let cmds = option
         .options
@@ -466,6 +494,10 @@ async fn download(
         return Err(LogError::new_slash("Select at least one training", aci.clone()));
     }
 
+    // filter out multiple
+    trainings.sort_by_key(|t| t.id);
+    trainings.dedup_by_key(|t| t.id);
+    trainings.sort_by_key(|t| t.date);
 
     // What to parse to
     let format = if let Some(f) = cmds
@@ -484,16 +516,101 @@ async fn download(
     aci.create_interaction_response(ctx, |r| {
         r.kind(InteractionResponseType::ChannelMessageWithSource);
         r.interaction_response_data(|d| {
+            d.flags(MessageFlags::EPHEMERAL);
             d.content("Loading...")
         })
     }).await?;
 
     let msg = aci.get_interaction_response(ctx).await?;
 
+    let mut log: Vec<String> = Vec::new();
+    let mut tds: Vec<TrainingData> = Vec::with_capacity(trainings.len());
+
+    for t in trainings {
+
+        let signups = t.get_signups(ctx).await.log_slash_reply(aci)?;
+        let mut sds: Vec<SignupData> = Vec::with_capacity(signups.len());
+
+        for s in signups {
+
+            let user = s.get_user(ctx).await.log_slash_reply(aci)?;
+
+            let member = match guild.member(ctx, user.discord_id()).await {
+                Ok(du) => du,
+                Err(_) => {
+                    log.push(format!(
+                        "Did not find user with id {} in discord guild. Skipped",
+                        user.discord_id()
+                    ));
+                    continue;
+                }
+            };
+
+            let roles = s
+                .get_roles(ctx)
+                .await
+                .log_slash_reply(aci)?
+                .into_iter()
+                .map(|r| r.repr)
+                .collect::<Vec<_>>();
+
+            sds.push(
+                SignupData {
+                user,
+                member,
+                roles,
+                comment: s.comment,
+                }
+            );
+        }
+
+        let available_roles = t
+            .all_roles(ctx)
+            .await
+            .log_slash_reply(aci)?;
+
+        tds.push(
+            TrainingData {
+                training: t,
+                available_roles,
+                signups: sds,
+            }
+        );
+    }
+
+    let dbtiers = db::Tier::all(ctx).await.log_slash_reply(aci)?;
+    let mut tiers: Vec<TierData> = Vec::with_capacity(dbtiers.len());
+
+    for t in dbtiers {
+        let dr = t
+            .get_discord_roles(ctx)
+            .await
+            .log_slash_reply(aci)?
+            .iter()
+            .map(|t| guild.roles.get(&RoleId::from(t.discord_role_id as u64)))
+            .collect::<Vec<_>>();
+
+        let mut includes: Vec<Role> = Vec::with_capacity(dr.len());
+
+        for r in dr {
+            match r {
+                Some(r) => includes.push(r.clone()),
+                None => log.push(format!("Failed to find a discord role for tier: {}", t.name)),
+            }
+        }
+
+        tiers.push( TierData {
+            id: t.id,
+            name: t.name,
+            includes,
+        })
+    }
+
     let data = DownloadData {
         output: format,
         created: chrono::Utc::now().naive_utc(),
-        trainings: Vec::new(),
+        trainings: tds,
+        tiers
     };
 
     let data_bytes = match data.output {
@@ -507,7 +624,7 @@ async fn download(
                 .into_bytes()
         },
         DonwloadFormat::Json => {
-            let json = serde_json::to_string(&data).log_slash_reply(aci)?;
+            let json = serde_json::to_string_pretty(&data).log_slash_reply(aci)?;
             json.as_bytes().to_vec()
         }
     };
@@ -517,12 +634,49 @@ async fn download(
         filename: String::from("signups.csv"),
     };
 
-    msg.channel_id.send_message(ctx, |m| {
+    let msg = msg.channel_id.send_message(ctx, |m| {
+        m.embed( |e| {
+            e.title("Download");
+            e.field(
+                "Details",
+                format!("Format: {}\nCreated: <t:{}>", data.output, data.created.timestamp()),
+                false
+            );
+            e.field(
+                "Trainings",
+                data
+                    .trainings
+                    .iter()
+                    .map(|t| format!(
+                            "\n__{}__\nId: {}\nData: <t:{}>",
+                            t.training.title,
+                            t.training.id,
+                            t.training.date.timestamp())
+                        )
+                    .collect::<Vec<_>>()
+                    .join("\n"),
+                false
+            );
+            e.field(
+                "Log",
+                match log.is_empty() {
+                    true => String::from("No errors"),
+                    false => {
+                        log
+                            .iter()
+                            .map(|s| format!("`{}`", s))
+                            .collect::<Vec<_>>()
+                            .join("\n")
+                    }
+                },
+                false
+            )
+        });
         m.add_file(file)
     }).await?;
 
-    aci.edit_followup_message(ctx, msg.id, |r| {
-        r.content("Done")
+    aci.edit_original_interaction_response(ctx, |r| {
+        r.content(format!("[Done]({})", msg.link()))
     }).await?;
 
     Ok(())
